af53e5582b7906c955dd4e1a97729f1f8b12e969
icpacifico <cordillerainmobiliaria.redes@gmail.com>
2022-10-27 09:14:36 -0300

    tags de facebook y google

--- resources/views/sitioweb/app.blade.php
+++ resources/views/sitioweb/app.blade.php
@@ -15,8 +15,47 @@
 	@vite(['resources/css/app.css', 'resources/js/app.js'])
 	
 	@yield('style')
+	<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PW4QL7Q');</script>
+	<!-- End Google Tag Manager -->
+
+	<!-- Facebook Pixel Code -->
+<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window, document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '1727543543972159');
+	fbq('track', 'PageView');
+	fbq('track', 'ViewContent');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=1727543543972159&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Facebook Pixel Code -->
+  
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+	(adsbygoogle = window.adsbygoogle || []).push({
+	  google_ad_client: "ca-pub-1054465047182212",
+	  enable_page_level_ads: true
+	});
+  </script>
+  
+  <script>function loadScript(a){var b=document.getElementsByTagName("head")[0],c=document.createElement("script");c.type="text/javascript",c.src="https://tracker.metricool.com/resources/be.js",c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:"1dcf1a21c87d82ad19000315ebd3dad7"})});</script>
+  
 </head>
 <body>
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PW4QL7Q"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div class="preloader" >
     <div class="loader">
         <div class="loader__figure"></div>
@@ -142,11 +181,7 @@
 			infinite: true,
 			slidesToShow: 1,
 			adaptiveHeight: true
-		});
-		// function agendarVideoLlamada(){
-		// 	dataLayer.push({'event': 'agenda-click'});
-			
-		// }
+		});		
 		$('.videollamada').on('click',function(){
 			$.fancybox.open({
 				src : 'https://calendly.com/icpacifico/reunion-virtual',
