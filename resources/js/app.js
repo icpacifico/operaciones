e21de7722eba48a92936af63a2109bfd3437b288
icpacifico <cordillerainmobiliaria.redes@gmail.com>
2022-11-02 13:28:55 -0300

    archivo de google

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 import './bootstrap';
-// import './jquery.min';
 import '../../node_modules/sweetalert2/dist/sweetalert2.css';
 import '../../node_modules/fontawesome-free-v6/css/all.min.css';
 import '../css/bootstrap-touch-slider.css';
@@ -15,18 +14,15 @@ import '../css/loader.css';
 import '../sliders/slider4.css';
 import '../sliders/slider_proyecto.css';
 import '../css/animate.css';
-// import '../../node_modules/unitegallery/dist/css/unite-gallery.css';
 import '../css/headers1-10.css';
 import '../css/jquery.fancybox.css';
 import '../../node_modules/@popperjs/core/dist/umd/popper.min';
-// import '../../node_modules/bootstrap/dist/js/bootstrap.min';
 import * as bootstrap from 'bootstrap';
 import './slide';
 import '../../node_modules/sweetalert2/dist/sweetalert2';
 import '../../node_modules/perfect-scrollbar/dist/perfect-scrollbar.min';
 import '../../node_modules/jquery-touchswipe/jquery.touchSwipe.min';
 import './jquery.fancybox';
-// import '../../node_modules/unitegallery/dist/js/unitegallery.min';
 import '../../node_modules/toastr/build/toastr.min';
 
 import.meta.glob([
